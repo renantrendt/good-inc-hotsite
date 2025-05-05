3294cf5ca6f5432b0f6d9c3334ea0ad34b93c751
Renan Serrano <renantrendt@gmail.com>
2025-05-05 15:59:01 -0700

    - google tag manager

--- app/layout.tsx
+++ app/layout.tsx
@@ -59,8 +59,27 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <link rel="canonical" href="https://www.good.inc" />
         <link rel="shortcut icon" href="/favicon.ico" />
         <script async src="https://cdn.streamform.io/streamform.js" data-token="adY1dF7oW9tmhOoQKc7nXjlYVeKA49DLiSCrBVdgFQA"></script>
+        {/* Google Tag Manager */}
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-TK9WMP62');`}
+        </Script>
+        {/* End Google Tag Manager */}
       </head>
       <body className={inter.className}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe 
+            src="https://www.googletagmanager.com/ns.html?id=GTM-TK9WMP62"
+            height="0" 
+            width="0" 
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         <LanguageProvider>{children}</LanguageProvider>
       </body>
     </html>
